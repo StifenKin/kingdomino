1c5314c302ff6a8261a47c196fc561bc4ba55ea3
StifenKin <tomaspfarias@gmail.com>
2021-06-23 09:50:59 -0300

    UpdateInterfazGrafica

--- KingDomino/src/InterfazGrafica/ImagenFondo.java
+++ KingDomino/src/InterfazGrafica/ImagenFondo.java
@@ -11,7 +11,7 @@ public class ImagenFondo extends JPanel{
 	    @Override
 	    public void paint(Graphics g){
 	        Dimension dimension = this.getSize();
-	        ImageIcon icon = new ImageIcon(getClass().getResource("/imagenes/prueba.jpg"));
+	        ImageIcon icon = new ImageIcon(getClass().getResource("/imagenes/kingdomino.png"));
 	        g.drawImage(icon.getImage(), 0, 0, dimension.width, dimension.height, null);
 	        setOpaque(false);
 	        super.paintChildren(g);
